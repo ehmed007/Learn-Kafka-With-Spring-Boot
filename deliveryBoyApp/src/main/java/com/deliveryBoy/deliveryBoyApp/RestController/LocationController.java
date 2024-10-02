8249447485590fc24ac0d56e8d6c9d392761c698
Ahmed Rasheed <ehmedmughal@gmail.com>
2024-10-02 03:14:48 -0700

    added dynamic data functionality

--- deliveryBoyApp/src/main/java/com/deliveryBoy/deliveryBoyApp/RestController/LocationController.java
+++ deliveryBoyApp/src/main/java/com/deliveryBoy/deliveryBoyApp/RestController/LocationController.java
@@ -5,10 +5,7 @@ import com.deliveryBoy.deliveryBoyApp.Service.KafkaService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.Map;
 
@@ -24,9 +21,9 @@ public class LocationController {
         return "Server is running on 8080 port";
     }
 
-    @GetMapping("/update")
-    public ResponseEntity<?> updateLocation() {
-        this.kafkaService.updateLocation("( "+Math.round(Math.random()*100)+" , " + Math.round(Math.random()*100) + ")");
+    @GetMapping("/update/{data}")
+    public ResponseEntity<?> updateLocation(@PathVariable String data) {
+        this.kafkaService.updateLocation(data);
         return new ResponseEntity<>(Map.of("message","Location Updated"), HttpStatus.OK);
     }
 }
